5791a669212af1cb5a2f36a498f150a03ac4faa3
jacmoe <jacmoe.dk@gmail.com>
2022-10-08 23:31:26 +0200

    Zen-mode plugin

--- lua/package-manager/init.lua
+++ lua/package-manager/init.lua
@@ -6,5 +6,15 @@ return require'packer'.startup(function()
 	        require('orgmode').setup{}
 	end}
 	use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+    use {
+  "folke/zen-mode.nvim",
+  config = function()
+    require("zen-mode").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
 end)
 
