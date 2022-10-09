95a37c42374ea0a5b946a0ce37a21ac00dfecea9
jacmoe <jacmoe.dk@gmail.com>
2022-10-09 14:17:08 +0200

    Nvim-lastplace plugin

--- lua/package-manager/init.lua
+++ lua/package-manager/init.lua
@@ -1,11 +1,12 @@
 return require'packer'.startup(function()
 	use 'wbthomason/packer.nvim'
 	use "EdenEast/nightfox.nvim"
-	use {'nvim-treesitter/nvim-treesitter'}
+	use 'nvim-treesitter/nvim-treesitter'
 	use {'nvim-orgmode/orgmode', config = function()
 	        require('orgmode').setup{}
 	end}
 	use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
-	use { 'junegunn/goyo.vim' }
+	use 'junegunn/goyo.vim'
+	use 'ethanholz/nvim-lastplace'
 end)
 
