b1c5f32fd8b6a9087ae13e019974e51bd1f00ee9
jacmoe <jacmoe.dk@gmail.com>
2022-10-11 01:32:54 +0200

    Galaxyline and Marks

--- init.lua
+++ init.lua
@@ -3,3 +3,5 @@ require('package-manager')
 require('themes')
 require('orgsetup')
 require('neogit-setup')
+require('marks-setup')
+
--- lua/package-manager/init.lua
+++ lua/package-manager/init.lua
@@ -8,6 +8,17 @@ return require'packer'.startup(function()
 	use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
 	use 'junegunn/goyo.vim'
 	use 'ethanholz/nvim-lastplace'
+	use 'chentoast/marks.nvim'
+	use({
+	  	'glepnir/galaxyline.nvim',
+		  branch = 'main',
+		  -- your statusline
+		  config = function()
+		    require('galaxyline-settings')
+		  end,
+		  -- some optional icons
+		  requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+	})
 	use { 'rmagatti/auto-session',
 		config = function()
 			require("auto-session").setup {
